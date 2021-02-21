65dbd6eb8a27eb2c1f592e7f295877bcb93d99b6
Loreto <0314lsolanes@e-itaca.es>
2021-02-21 15:44:23 +0100

    Interfaz del login terminada.
    Se muestra y oculta la contraseña.
    Si los campos del username y el password están vacíos sale un cartel y se ponen rojos.

--- guiFx/src/main/java/OpenJFXInterfaz/guiFx/LoginController.java
+++ guiFx/src/main/java/OpenJFXInterfaz/guiFx/LoginController.java
@@ -3,18 +3,24 @@ package OpenJFXInterfaz.guiFx;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
 public class LoginController {
 	
 	@FXML
-	private PasswordField usernameField;
+	private TextField usernameField;
+	@FXML
+	private TextField showPassField;
 	@FXML
 	private PasswordField passField;
 	@FXML
 	private Text textObligatoryFields;
+	@FXML
+	private CheckBox showPassBox;
 	
 	
 	@FXML
@@ -22,24 +28,40 @@ public class LoginController {
         Platform.exit();
     }
 
-//	@FXML
-//	public void okButton(MouseEvent  event) {
-//		if((usernameField.getText().isEmpty()) || (passField.getText().isEmpty())) {
-//			textObligatoryFields.setVisible(true);
-//		}
-//		if(usernameField.getText().isEmpty()) {
-//			usernameField.setStyle("-fx-background-color: #b51313");
-//		}
-//		if(passField.getText().isEmpty()) {
-//			passField.setStyle("-fx-background-color: #b51313");
-//		}
-//	}
+	@FXML
+	public void okButton(MouseEvent  event) {
+		if((usernameField.getText().isEmpty()) || (passField.getText().isEmpty())) {
+			textObligatoryFields.setVisible(true);
+		}else {
+			textObligatoryFields.setVisible(false);
+		}
+		if(usernameField.getText().isEmpty()) {
+			usernameField.setStyle("-fx-background-color: #b51313");
+		}else {
+			usernameField.setStyle("-fx-background-color: #FFFFFF");
+		}
+		if(passField.getText().isEmpty()) {
+			passField.setStyle("-fx-background-color: #b51313");
+		}else {
+			passField.setStyle("-fx-background-color: #FFFFFF");
+		}
+	}
 	
-//	@FXML
-//	public void showPassword(MouseEvent  event) {		//Muestro la contrasenha
-//        System.out.println("You clicked me!");
+	@FXML
+	public void showPassword(MouseEvent  event) {		//Muestro la contrasenha y la vuelvo a ocultar si haces clic en el checkbox
+        String password = passField.getText();			//Guardo el texto de la contrasenha
+        if(showPassBox.isSelected() == true) {
+        	showPassField.setText(password);
+        	showPassField.setVisible(true);
+        	passField.setVisible(false);
+        }else {        	
+        	showPassField.setVisible(false);
+        	passField.setVisible(true);        	
+        }
+        password = showPassField.getText();				//Guardo el texto de la contrasenha escrito en el TextField
+        passField.setText(password);					//Escribo el texto guardado en el PasswordField
         
-//        passField.setText(password.getText());
+//        passField.setText(passField.getText());
 
-//    }
+    }
 }
